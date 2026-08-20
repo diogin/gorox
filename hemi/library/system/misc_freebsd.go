30917d9a2aac853e71ceecc17d6fb99c7119eb6b
Zhang Jingcheng <diogin@gmail.com>
2026-08-20 23:33:58 +0800

    year 2026

--- hemi/library/system/misc_freebsd.go
+++ hemi/library/system/misc_freebsd.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2025 Zhang Jingcheng <diogin@gmail.com>.
+// Copyright (c) 2020-2026 Zhang Jingcheng <diogin@gmail.com>.
 // All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
